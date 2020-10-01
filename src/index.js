f2f5def3d6d7fa4df756d3a1b5c04a20f0e28d71
Talai Sergei <tsa@tut.by>
2020-10-01 21:16:32 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return !array || !Array.isArray(array) || array.length == 0 ? 0 : Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+    return !array || !Array.isArray(array) || array.length == 0 ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return !array || !Array.isArray(array) || array.length == 0 ? 0 : array.reduce((el, sum) => el + sum) / array.length;
 }
